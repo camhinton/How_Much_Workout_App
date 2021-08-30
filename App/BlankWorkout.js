2fb2536d33b1880d951ab9e8c7109e733633990a
Minchan Han <minchanhan5@gmail.com>
2021-08-30 00:26:32 -0400

    blank workout test page

---
+++ App/BlankWorkout.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { SafeAreaView } from "react-native";
+
+export default class BlankWorkout extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>Blank Workout</Text>
+      </SafeAreaView>
+    );
+  }
+}
