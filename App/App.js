6982458438577194b6165c4210878c2914b2923b
Cameron <camhinton@gmail.com>
2021-08-30 00:43:58 -0400

    Added navigation and added the home screen component

--- App/App.js
+++ App/App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Image, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, Image, Text, SafeAreaView, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen } from './src/HomeScreen';
+
+
+const stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.white}>How Much?</Text>
-      <Image
-        style={styles.logo}
-        source={require('./assets/how_much.jpg')} 
-      />
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Home"
+          component={HomeScreen}
+        />
+        <Stack.Screen 
+          name="WorkoutHistory"
+          component={HomeScreen}
+        />
+        <Stack.Screen 
+          name="WorkoutPlans"
+          component={HomeScreen}
+        />
+        <Stack.Screen 
+          name="WorkoutTracker"
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000000',
