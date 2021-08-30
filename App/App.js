a04f0d8360024a60e6ab70a61e2947fd1e83170a
Cameron <camhinton@gmail.com>
2021-08-29 23:45:29 -0400

    Added logo

--- App/App.js
+++ App/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, Image, Text, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.white}>How Much?</Text>
-      <Image></Image>
+      <Image source={require('./assets/how_much.jpg')} />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
